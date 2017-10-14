63309d733098219e88ec2917d0bd6ca42d1c58f4
YumnaAsim <yumnaasim11@gmail.com>
2017-10-14 11:47:24 +0500

    made splash screen

--- app/src/main/java/com/example/yumnaasim/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/yumnaasim/myapplication/SecondActivity.java
@@ -3,12 +3,11 @@ package com.example.yumnaasim.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        this.setTitle("Droid Tech");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
     }
 }
